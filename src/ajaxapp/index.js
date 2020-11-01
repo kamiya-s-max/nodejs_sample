f661809adbe0aeda70797891c83a6b075da71af6
kamiya-s-max <s.kamiya.f15@gmail.com>
2020-11-01 11:45:31 +0900

    view表示する処理を別関数に切り出し

--- src/ajaxapp/index.js
+++ src/ajaxapp/index.js
@@ -9,20 +9,8 @@ function fetchUserInfo(userId) {
       console.error("エラーレスポンス", response);
     } else {
       return response.json().then(userInfo => {
-        const view = createView(userInfo)
-        // escapeHTML`
-        // <h4>${userInfo.name} (@${userInfo.login})</h4>
-        // <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-        // <dl>
-        //   <dt>Location</dt>
-        //   <dd>${userInfo.location}</dd>
-        //   <dt>Repositories</dt>
-        // <dd>${userInfo.public_repos}</dd>
-        // </dl>
-        // `;
-
-        const result = document.getElementById("result");
-        result.innerHTML = view;
+        const view = createView(userInfo);
+        displayView(view);
       });
     }
   }).catch(error => {
@@ -41,6 +29,11 @@ function createView(userInfo) {
   <dd>${userInfo.public_repos}</dd>
   </dl>
   `;
+}
+
+function displayView(view) {
+  const result = document.getElementById("result");
+  result.innerHTML = view;
 }
 
 
